622ef6ad75bf22a08d0b3cc7002cd12f0a7dc32b
Tilahun Assefa Demissie <til.assf98@gmail.com>
2022-07-14 00:41:38 -0500

    refactor router and add controller

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 
 //import router module
-const v1Router = require("./v1/routes");
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// For testing purposes using baseURL and router 
-app.use("/api/v1", v1Router);
+// For testing purposes using baseURL and router(http://localhost:3000/api/v1/workouts) 
+app.use("/api/v1/workouts", v1WorkoutRouter);
 
 app.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`);
